7a97d2ac3f3ed86075b64ac6da0774299f3c6095
Georgeta Bordea <georgeta.bordea@u_bordeaux.fr>
2017-10-05 14:14:30 +0200

    Added class for extracting DBpedia food categories

---
+++ src/main/java/fr/ubx/bph/erias/food/DBpediaFoodExtractor.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package fr.ubx.bph.erias.food;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+/**
+ * @author Georgeta Bordea
+ *
+ */
+public class DBpediaFoodExtractor {
+
+	public void downloadPage(String seedPageURI) {
+		Document doc;
+		try {
+			doc = Jsoup.connect(seedPageURI).get();
+	        String text = doc.body().text();
+
+	        System.out.print(text);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
