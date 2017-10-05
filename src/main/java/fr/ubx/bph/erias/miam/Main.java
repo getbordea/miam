ab54b51d81ba296b8127cda009b5070d1ac857f8
Georgeta Bordea <georgeta.bordea@u_bordeaux.fr>
2017-10-05 15:01:06 +0200

    Added Eclipse formatting file

--- src/main/java/fr/ubx/bph/erias/miam/Main.java
+++ src/main/java/fr/ubx/bph/erias/miam/Main.java
@@ -1,1 +1,13 @@
 package fr.ubx.bph.erias.miam;
+
+import fr.ubx.bph.erias.miam.food.DBpediaFoodExtractor;
+
+public class Main {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		DBpediaFoodExtractor dfe = new DBpediaFoodExtractor();
+		dfe.downloadPageHTML("http://dbpedia.org/page/Category:Foods");
+	}
+}
--- src/main/java/fr/ubx/bph/erias/miam/food/DBpediaFoodExtractor.java
+++ src/main/java/fr/ubx/bph/erias/miam/food/DBpediaFoodExtractor.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 /**
  * @author Georgeta Bordea
@@ -14,13 +16,17 @@ import org.jsoup.nodes.Document;
  */
 public class DBpediaFoodExtractor {
 
-	public void downloadPage(String seedPageURI) {
+	public void downloadPageHTML(String seedPageURI) {
 		Document doc;
+		Elements narrowerCategoryElements;
 		try {
 			doc = Jsoup.connect(seedPageURI).get();
-	        String text = doc.body().text();
+			narrowerCategoryElements = doc.body().getElementsByAttributeValue("rev", "skos:broader");
 
-	        System.out.print(text);
+			for (Element element : narrowerCategoryElements) {
+				System.out.print(element.text() + " ");
+				System.out.println(element.attr("href"));
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
