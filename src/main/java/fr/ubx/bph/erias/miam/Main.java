3a7ab3a076028ab09c34d7f74f8264c93a82e975
Georgeta Bordea <georgeta.bordea@u_bordeaux.fr>
2017-10-06 11:19:56 +0200

    Branching for development and started work on issue 1

--- src/main/java/fr/ubx/bph/erias/miam/Main.java
+++ src/main/java/fr/ubx/bph/erias/miam/Main.java
@@ -1,13 +1,28 @@
 package fr.ubx.bph.erias.miam;
 
+import java.io.IOException;
+
 import fr.ubx.bph.erias.miam.food.DBpediaFoodExtractor;
 
 public class Main {
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		DBpediaFoodExtractor dfe = new DBpediaFoodExtractor();
-		dfe.downloadPageHTML("http://dbpedia.org/page/Category:Foods");
-	}
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    DBpediaFoodExtractor dfe = new DBpediaFoodExtractor();
+
+    final String SEED = "http://dbpedia.org/page/Category:Foods";
+
+    System.out.println("Extracting DBpedia subcategories for " + SEED);
+
+    dfe.recursiveDownloadNarrowerCategories(SEED);
+    
+    //try {
+    //  dfe.downloadNarrowerCategories(SEED);
+    //} catch (IOException e) {
+    //  e.printStackTrace();
+    //}
+    
+    System.out.println("Done.");
+  }
 }
--- src/main/java/fr/ubx/bph/erias/miam/food/DBpediaFoodExtractor.java
+++ src/main/java/fr/ubx/bph/erias/miam/food/DBpediaFoodExtractor.java
@@ -3,7 +3,14 @@
  */
 package fr.ubx.bph.erias.miam.food;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -16,19 +23,66 @@ import org.jsoup.select.Elements;
  */
 public class DBpediaFoodExtractor {
 
-	public void downloadPageHTML(String seedPageURI) {
-		Document doc;
-		Elements narrowerCategoryElements;
-		try {
-			doc = Jsoup.connect(seedPageURI).get();
-			narrowerCategoryElements = doc.body().getElementsByAttributeValue("rev", "skos:broader");
+  private static final String OUTPUT_FILE = "output/dbPediaCategories.txt";
 
-			for (Element element : narrowerCategoryElements) {
-				System.out.print(element.text() + " ");
-				System.out.println(element.attr("href"));
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+  public List<String> downloadNarrowerCategories(String seedPageURI)
+      throws IOException {
+    Document doc;
+    Elements narrowerCategoryElements;
+
+    FileWriter fstream;
+    BufferedWriter out = null;
+
+    List<String> categoryURLList = new ArrayList<String>();
+
+    String seedCategoryName =
+        seedPageURI.substring(seedPageURI.lastIndexOf(':') + 1);
+
+    try {
+
+      fstream = new FileWriter(OUTPUT_FILE, false);
+      out = new BufferedWriter(fstream);
+
+      doc = Jsoup.connect(seedPageURI).get();
+      narrowerCategoryElements =
+          doc.body().getElementsByAttributeValue("rev", "skos:broader");
+
+      for (Element element : narrowerCategoryElements) {
+        // System.out.print(element.text() + " ");
+
+        String categoryURL = element.attr("href");
+        String categoryName =
+            categoryURL.substring(categoryURL.lastIndexOf(':') + 1);
+        out.write("  \"" + seedCategoryName + "\"" + " -> " + "\"" + categoryName
+            + "\";" + System.lineSeparator());
+
+        System.out.println(seedCategoryName + " " + categoryName);
+
+        categoryURLList.add(categoryURL);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      // Close the output stream
+      if (out != null) {
+        out.close();
+      }
+    }
+
+    return categoryURLList;
+  }
+
+  public void recursiveDownloadNarrowerCategories(String seedPageURI) {
+
+    List<String> narrowerCategories;
+    try {
+      narrowerCategories = downloadNarrowerCategories(seedPageURI);
+
+      for (String dbPediaConceptURI : narrowerCategories) {
+        recursiveDownloadNarrowerCategories(dbPediaConceptURI);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
