be9071a1ad16db1bc05d4d348e3d95026019d77c
Georgeta Bordea <georgeta.bordea@u_bordeaux.fr>
2017-10-06 11:34:41 +0200

    Cleaned imports

--- src/main/java/fr/ubx/bph/erias/miam/food/DBpediaFoodExtractor.java
+++ src/main/java/fr/ubx/bph/erias/miam/food/DBpediaFoodExtractor.java
@@ -3,10 +3,7 @@
  */
 package fr.ubx.bph.erias.miam.food;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
